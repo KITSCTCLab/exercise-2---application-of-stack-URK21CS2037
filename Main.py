523d020b36ebf385768af54534f7f3de070cf948
URK21CS2037 <109648094+URK21CS2037@users.noreply.github.com>
2022-08-03 10:52:20 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,64 +1,59 @@
 class Evaluate:
- """This class validates and evaluates postfix expressions.
-Attributes:
-top: An integer which denotes the index of the element at the top of the stack currently.
-size_of_stack: An integer which represents the size of stack.
-stack: A List which acts as a Stack.
-"""
  def __init__(self, size):
-   self.top = -1
-   self.size_of_stack = size
-   self.stack = []
+  self.top = -1
+  self.size_of_stack = size
+  self.stack = []
 
  def isEmpty(self):
-   if self.top == -1:
-      return True
-   else:
-      return False
+  if self.top == -1:
+   return True
+  else:
+   return False
 
  def pop(self):
-   if not self.isEmpty():
-     self.stack.pop()
+  if not self.isEmpty():
+   self.stack.pop()
 
  def push(self, operand):
   if self.top != self.size_of_stack - 1:
    self.stack.append(operand)
 
-def validate_postfix_expression(self, expression):
- x = 0
- y = 0
-for element in expression:
- if element.isnumeric():
-  x = x + 1
- else:
-  y = y + 1
- if y == x - 1:
-  return True
- else:
-  return False
+ def validate_postfix_expression(self, expression):
+  x = 0
+  y = 0
+  for element in expression:
+   if element.isnumeric():
+    x = x + 1
+   else:
+    y = y + 1
+   if y == x - 1:
+    return True
+   else:
+    return False
 
-def evaluate_postfix_expression(self, expression):
- stack = []
- for i in expression:
-  if i.isnumeric():
-   stack.append(int(i))
-  if len(stack) >= 2:
-   if i == '+':
-    stack[-2] = stack[-2] + stack[-1]
-    stack.pop()
-   elif i == '-':
-    stack[-2] = stack[-2] - stack[-1]
-    stack.pop()
-   elif i == '*':
-    stack[-2] = stack[-2] * stack[-1]
-    stack.pop()
-   elif i == '/':
-    stack[-2] = stack[-2] / stack[-1]
-    stack.pop()
-   elif i == '^':
-    stack[-2] = stack[-2] ^ stack[-1]
-    stack.pop()
-return int(stack[-1])
+ def evaluate_postfix_expression(self, expression):
+  stack = []
+  for i in expression:
+   if i.isnumeric():
+    stack.append(int(i))
+   if len(stack) >= 2:
+    if i == '+':
+     stack[-2] = stack[-2] + stack[-1]
+     stack.pop()
+    elif i == '-':
+     stack[-2] = stack[-2] - stack[-1]
+     stack.pop()
+    elif i == '*':
+     stack[-2] = stack[-2] * stack[-1]
+     stack.pop()
+    elif i == '/':
+     stack[-2] = stack[-2] / stack[-1]
+     stack.pop()
+    elif i == '^':
+     stack[-2] = stack[-2] ^ stack[-1]
+     stack.pop()
+  return int(stack[-1])
+  
 
 # Do not change the following code
 postfix_expression = input() # Read postfix expression
